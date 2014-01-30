4fd159d94170b55dd33cb53c09da66ddd5c0b841
tservArk <tservati@gmail.com>
2014-01-29 20:08:58 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TBJCreations::Application.config.secret_key_base = '7c162985cfadb85e05f6967c596c2220a779d3731b096a12e94d2d795c011c76cf9b4e04efeaf3610396f7d485839f366e01e5eb3fd12ad4d0c77c7283008dc8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TBJCreations::Application.config.session_store :cookie_store, key: '_TBJCreations_session'
